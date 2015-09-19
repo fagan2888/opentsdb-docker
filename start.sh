08d8c22d00c092f6421c646579b8129eab7beca6
Antonio J F Sousa <antonio@tradingeconomics.com>
2015-09-19 18:13:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ echo 'OPENTSDB ZK SET TO' $HBASE_ZK
 
 /usr/share/opentsdb/bin/tsdb tsd --zkquorum=$HBASE_ZK --auto-metric
 
-echo 'Leave a loop scrip running'
-/bin/sh -c "while true; do echo 'OPENTSDB RUNNING'; /bin/date; sleep 1; done" 
+#echo 'Leave a loop scrip running'
+#/bin/sh -c "while true; do echo 'OPENTSDB RUNNING'; /bin/date; sleep 1; done" 
