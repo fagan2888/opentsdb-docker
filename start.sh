77d937a555885b06e8c861a1df8fd8e0309a94f5
EC2 Default User <ec2-user@ip-10-0-1-25.ec2.internal>
2015-09-23 13:47:18 +0000

    -

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo 'STARTING OPENTSDB'
+echo 'RUNNING OPENTSDB'
+echo 'POINTING TO HBASE ZK ' $HBASE_ZK
 
-echo 'OPENTSDB ZK SET TO' $HBASE_ZK
-
-echo 'WAIT A BIT FOR HBASE TO START'
+echo 'WAIT A BIT FOR HBASE TO WARMUP'
 sleep 30
 
 echo 'LAUNCHING OPENTSDB'
-/usr/share/opentsdb/bin/tsdb tsd --zkquorum=$HBASE_ZK --auto-metric
+/opt/start-tsdb.sh
 
 #echo 'Leave a loop scrip running'
 #/bin/sh -c "while true; do echo 'OPENTSDB RUNNING'; /bin/date; sleep 1; done" 
